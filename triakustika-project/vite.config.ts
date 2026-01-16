249aac7bee9b2c4db7fae8ee650b67886314b910
tatasutaryat-dotcom <tatasutaryat@gmail.com>
2026-01-17 00:16:55 +0700

    Update vite.config.ts

--- triakustika-project/vite.config.ts
+++ triakustika-project/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
